876bc24901c31ff4c327564b57bc735ffe586e23
Adrien <acere@linagora.com>
2026-07-08 18:37:35 +0200

    feat(catalog-ui): disable create button when no filters are present

--- apps/catalog-ui/src/components/smart-filter/LinidSmartFilter.vue
+++ apps/catalog-ui/src/components/smart-filter/LinidSmartFilter.vue
@@ -114,7 +114,7 @@
 
         <linid-favorite-panel
           :favorites="options?.filterSets ?? []"
-          :filters="options?.filters ?? []"
+          :filters="props?.filters ?? []"
           :ui-namespace="localUiNamespace"
           :i18n-scope="localI18nScope"
           data-cy="linid-smart-favorite-panel"
